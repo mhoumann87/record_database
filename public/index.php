fb3965ae669a437660638bbb4028ad344a899d35
Michael Houmann <mhoumann87@gmail.com>
2021-12-29 16:19:22 +0100

    Update home page :recycle:

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 
+/* This page exists to prevent unwanted persons to access the root folder */
+
 require_once '../private/initialize.php';
+
+$page_title = 'Home';
+
+include SHARED_PATH . '/public-header.php';
 
 
 ?>

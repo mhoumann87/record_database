63a36ec88a0bf0f6d8a9bf21471bae5acfd5f1d4
Michael Houmann <mhoumann87@gmail.com>
2021-12-29 12:48:44 +0100

    Add start of initialization.php :sparkles:

---
+++ private/initialization.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Turn on output buffering 
+ */
+ob_start();
+
+/*
+ * Display PHP errors on the pages
+ ! REMEMBER TO DELETE THIS BEFORE UPLOADING TO SERVER 
+*/
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+

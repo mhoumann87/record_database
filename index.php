db40147e912774f7161944d207fc98c19ebecff5
Michael Houmann <mhoumann87@gmail.com>
2021-12-29 16:16:10 +0100

    Update private index files :recycle:

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+/* This page exists to prevent unwanted persons to access the root folder */
+
 require_once './private/initialize.php';
 
-redirect_to('public/index.php');
+redirect_to('/record-database/public/index.php');
---
+++ private/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'initialize.php';
+
+/* 
+ * This file only exists to prevent unwanted persons to accessing the private folder
+*/
+
+redirect_to('/record-database/public/index.php');

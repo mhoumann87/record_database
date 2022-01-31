6dd5163701e87f59c4434c5b4fa40ec531cb6cfd
Michael Houmann <mhoumann87@gmail.com>
2022-01-31 13:18:57 +0100

    Add admin header

---
+++ private/shared/admin_header.php
@@ -0,0 +1,18 @@
+<?php if (!isset($page_title)) {
+  $page_title = 'Admin Area';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Record Database - <?php echo $page_title ?></title>
+</head>
+
+<body>
+  <header>
+    <h1>Admin Header</h1>
+  </header>

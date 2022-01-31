8ed7843ba08e6a131f417188721fb83049f93cc6
Michael Houmann <mhoumann87@gmail.com>
2022-01-31 14:18:53 +0100

    Add nav to user pages and function to create links :sparkles:

--- private/functions.php
+++ private/functions.php
@@ -1,5 +1,15 @@
 <?php
 
+/* Function to create links */
+function url_for($script_path)
+{
+  // add the leading // if missing in path
+  if ($script_path[0] != '/') {
+    $script_path = '/'.$script_path;
+  }
+  return WWW_ROOT.$script_path;
+}
+
 /* Function to redirect to another page */
 function redirect_to($location)
 {
--- public/admin/users/show.php
+++ public/admin/users/show.php
@@ -5,3 +5,17 @@
 <?php include SHARED_PATH.'/admin_header.php'; ?>
 
 <h2>Show User</h2>
+
+<nav aria-label="secondary navigation">
+  <ul>
+    <li><a href="<?php echo url_for('/admin/users/index.php') ?>">Show all users</a></li>
+
+    <li><a href="<?php echo url_for('/admin/users/new.php') ?>">Create user</a></li>
+
+    <li><a href="<?php echo url_for('/admin/users/delete.php') ?>">Delete user</a></li>
+
+    <li><a href="<?php echo url_for('/admin/users/edit.php') ?>">Edit user</a></li>
+
+    <li><a href="<?php echo url_for('/admin/users/show.php') ?>">Show user</a></li>
+    </ul>
+</nav>

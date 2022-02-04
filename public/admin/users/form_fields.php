7772fe06248c8e2b7971602d013c4a84d34c4513
Michael Houmann <mhoumann87@gmail.com>
2022-02-04 13:16:06 +0100

    Add form fields for users :sparkles:

---
+++ public/admin/users/form_fields.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * Prevents this code from being loaded in the browser
+ * without first setting the correct object½
+*/
+  if (!isset($user)) {
+    redirect_to(url_for('/admin/users/index.php'));
+  }
+?>
+
+
+<label for="user[username]">Username:
+  <input 
+      type="text" 
+      name="user[username]"
+      value="<?php echo h($user->username); ?>"
+      autofocus
+      />
+</label>
+
+<label for="user[first_name]">First Name:
+  <input 
+    type="text"
+    name="user[first_name]"
+    value="<?php echo h($user->first_name); ?>"
+  />
+</label>
+
+<label for="user[last_name]">Last Name:
+  <input 
+    type="text"
+    name="user[last_name]"
+    value="<?php echo h($user->last_name); ?>"
+    />
+</label>
+
+<label for="user[email]">Email:
+  <input 
+    type="text"
+    name="user[email]"
+    value="<?php echo h($user->email); ?>"
+  />
+</label>
+
+<label for="user[password]">Password:
+  <input 
+    type="password"
+    name="user[password]"
+  />
+</label>
+
+<label for="user[confirm_password]">Confirm Password:
+  <input 
+    type="password"
+    name="user[confirm_password]"
+  />
+</label>
+
+<!-- TODO! This check box is only for admins to check -->
+  
+<label for="user[is_admin]">Administrator:
+  <input 
+    type="radio"
+    name="user[is_admin]"
+    value="0"
+    <?php echo ($user->is_admin == 0) ? 'checked="checked"' : ''; ?>
+  />
+  No&nbsp;
+
+  <input
+    type="radio"
+    value="1"
+    <?php echo ($user->is_admin == 1) ? 'checked="checked"' : ''; ?>
+    />Yes
+</label>
+
